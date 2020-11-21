f51522b55269941c4815b13876cec05c200140bb
rhladchenko <ruslan.hladchenko@cpcs.ws>
2020-11-21 23:04:15 +0200

    Change the project

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combinedreducers, combineReducers } from 'redux';
+
 const songsReducer = () => {
     return [
         { title: 'No Scrubs', duration: '4:05' },
@@ -8,9 +10,14 @@ const songsReducer = () => {
 };
 
 const selectedSongReducer = (selectedSong = null, action) => {
-    if(action.type === 'SONG_SELECTED') {
+    if (action.type === 'SONG_SELECTED') {
         return action.payload;
     }
 
     return selectedSong;
 };
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
