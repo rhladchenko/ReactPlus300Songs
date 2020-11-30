cf3df9940fcf2df776d6531242fadeb2db3822e0
rhladchenko <ruslan.hladchenko@cpcs.ws>
2020-11-30 19:47:49 +0200

    Change the project

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,14 @@ import SongList from './SongList';
 
 const App = () => {
     return (
-        <div className="app">
-            <SongList />
+        <div className='app ui container grid'>
+            <div className='ui row'>
+                <div className='column eight wide'>
+                    <SongList />
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default App;
